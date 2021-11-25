004ca0197cb5955dc5ec5a72b24d03e3360440d1
alireza <alireza.mhm01@gmail.com>
2021-11-25 23:43:16 +0330

    update

--- engine/scripts/main.js
+++ engine/scripts/main.js
@@ -12,7 +12,7 @@ const root = document.querySelector(":root");
 const tooltipElements = document.querySelectorAll("[data-bs-toggle='tooltip']");
 const sidebarProfile = document.querySelector("#sidebar-profile");
 const mainSidebar = document.querySelector("#main-sidebar");
-
+const siteViewVhart = document.querySelector("#site-view-chart");
 
 // active tooltips
 for (let element of tooltipElements) {
@@ -59,14 +59,16 @@ function checkWindowWidth() {
     windowWidth = getWindowWidth();
     if (windowWidth < 768) {
         changeContentWidth("100%");
-        sidebarProfileInstance.show()
+        sidebarProfileInstance.show();
+        siteViewVhart.setAttribute("height", "75%")
     } else {
         if (sidebarStatus == "open") {
             changeContentWidth("calc(100% - 270px)");
         } else {
             changeContentWidth("100%");
         }
-        sidebarProfileInstance.hide()
+        sidebarProfileInstance.hide();
+        siteViewVhart.setAttribute("height", "25%")
     }
 }
 window.addEventListener("resize", checkWindowWidth);
